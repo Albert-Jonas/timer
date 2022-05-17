043f5159fc659249e2412bdf851f0baa7028ddad
Albert <jonas.albert.oktatas@gmail.com>
2022-05-17 20:34:32 +0200

    Új tesztek

--- tests/timer/TimerTest.java
+++ tests/timer/TimerTest.java
@@ -2,10 +2,16 @@ package timer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class TimerTest {
+
+	int seconds;
 
 	@ParameterizedTest(name = "{index} => seconds={0}, expected={1}")
 	@CsvSource({
@@ -19,4 +25,22 @@ class TimerTest {
 		assertEquals(expected, timer.toString());
 	}
 
+	@BeforeEach
+	void setUp() {
+		seconds = 0;
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	@DisplayName ("Empty test")
+	void testToString() {
+
+		Timer timer = new Timer(seconds);
+
+		assertEquals("00:00:00", timer.toString());
+
+	}
 }
